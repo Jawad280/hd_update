ffcc031a517803ad68d1efe27d2269d924f19214
Jawad <e0969647@u.nus.edu>
2024-12-02 14:34:23 +0700

    Celery logging explicit enabling

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@
 uvicorn __init__:create_app --host 0.0.0.0 --port 8000 &
 
 # Start celery
-celery -A celery_app.celery_app worker -l info --logfile=- &
+celery -A celery_app.celery_app worker -l info -E --logfile=- &
 
 wait
